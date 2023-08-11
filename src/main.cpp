d85bcf07ba6da34f5744a2bc8505c01afe928595
Vishesh Prasad <64453315+VisheshP-29@users.noreply.github.com>
2023-08-11 17:42:09 -0500

    Call python code using system calls

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,17 @@
 
 int main() {
     std::cout << "Comparing Trading Algorithms Personal Project" << std::endl;
+
+    /*
+     * Calling python code: Option 2
+     * Using system calls
+     */
+    system("python3 ../src/data_processing/DataProcessing.py");
+
+    /*
+     * Calling python code: Option 2
+     * In progress ...
+     */
+
     return 1;
 }
