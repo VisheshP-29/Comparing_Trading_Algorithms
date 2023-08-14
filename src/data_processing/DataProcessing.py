e2dfe68de3cc9dc23135e3ec1c4678397faf900b
Vishesh Prasad <64453315+VisheshP-29@users.noreply.github.com>
2023-08-13 19:53:57 -0500

    Connect to MySQL server and create database

--- src/data_processing/DataProcessing.py
+++ src/data_processing/DataProcessing.py
@@ -1,9 +1,36 @@
-# import mysql.connector
+def clean():
+    # Import mysql.connector
+    try:
+        import mysql.connector
+    except:
+        print("Error with importing mysql.connector")
+        return
 
-def test():
-    print("Test Python")
-    print("1, 2, 3")
+    # Connecting to mysql server
+    try:
+        mydb = mysql.connector.connect(
+            host = "localhost",
+            user = "Comparing_Trading_Algorithms",
+            password = "password"
+        )
+    except:
+        print("Error with connecting to MySQL server")
+        return
+
+    # Set cursor for user
+    mycursor = mydb.cursor()
+
+    # Create necessary database if not already created
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS comparing_trading_algorithms_db")
+
+    # Display All Databases user has access to
+    #mycursor.execute("SHOW DATABASES")
+    #for x in mycursor:
+    #    print(x)
+
     return
 
+# Entry point for DataProcesssing.py
+# Calls helper function clean() to clean data and place in database
 if __name__ == '__main__':
-    test()
+    clean()
